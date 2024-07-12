f32bf864ddb7f27da93db1ce3421a0fe40601a5f
kkkarthik17 <32122460+kkkarthik17@users.noreply.github.com>
2024-07-12 18:49:18 +0530

    nav commit-1

--- materialui/src/App.js
+++ materialui/src/App.js
@@ -1,10 +1,12 @@
 import Leftside from './components/Leftside';
 import Main from './components/Main';
 import Rightside from './components/Rightside';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+     <NavBar />
      <Leftside />
      <Main />
      <Rightside />
